70e1541f90abbf1eaa37a90448f2de4f341db401
Luís Felipe <luis.fe8000@gmail.com>
2025-10-13 15:04:55 -0300

    atualizando style e html

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.getElementById("agua").addEventListener("mouseleave", function() {
+//         this.src = "imagenseconomiasustentavel/agua.png";
+//     });
+
+//     document.getElementById("planta").addEventListener("mouseleave", function() {
+//         this.src = "imagenseconomiasustentavel/planta.png";
+//     });
